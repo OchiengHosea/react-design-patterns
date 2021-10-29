0e1e2b8db5729290211098036e1e53852b070d17
OchiengHosea <jrhosea8@gmail.com>
2021-10-29 21:40:41 +0300

    Styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {SplitScreen} from "./SplitScreen";
+const LeftHandComponent = ({name}) => {
+  return <h1 style={{backgroundColor:"red"}}>{name}</h1>
+}
 
+const RigthHandComponent = ({message}) => {
+  return <h2 style={{backgroundColor:"green"}}>{message}</h2>
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplitScreen leftWeight={1} rightWeight={3}>
+      <LeftHandComponent name={"Left"}/>
+      <RigthHandComponent message={"right"}/>
+    </SplitScreen>
   );
 }
 
